2efc4c69327fc23282f945eab1aacffe8c0b44a2
danielmichaels <dans.address@outlook.com>
2022-01-28 11:49:03 +1100

    Update the link to use correct url
    Signed-off-by: danielmichaels <dans.address@outlook.com>

--- components/landing-page/Banner.jsx
+++ components/landing-page/Banner.jsx
@@ -188,10 +188,10 @@ export default function Banner() {
                       <div className="mt-3 sm:mt-0 sm:ml-3">
                         <div className="rounded-md shadow">
                           <a
-                            href="/register"
+                            href="/contact"
                             className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
                           >
-                            Get in contact
+                            Contact
                           </a>
                         </div>
                       </div>
