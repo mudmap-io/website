81b7ba914666e4608cfee75f5564cb501fc2b6a0
danielmichaels <dan@danielms.site>
2022-03-29 10:33:20 +1100

    Update content with post describing new hostname support.

--- components/landing-page/NewFeaturesBanner.jsx
+++ components/landing-page/NewFeaturesBanner.jsx
@@ -2,8 +2,12 @@ import {SpeakerphoneIcon} from "@heroicons/react/outline";
 
 const NewFeaturesBanner = () => {
 
-  const post = "/version-2-launched"
-  const blog = "/blog" + post
+  // the banner* items are dangerouslySetInnerHTML
+  const bannerText = "WAN routable Hostnames added!"
+  const mobileBannerText = "Device registrations now accept <strong>both</strong> hostnames and static IPs. Firewall's with a dynamic IP are now supported!"
+  // Setting post to "" will not render the blog button
+  const post = "hostnames-are-now-supported"
+  const blog = "/blog/" + post
   return (
     <div className="bg-indigo-600">
       <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
@@ -13,18 +17,23 @@ const NewFeaturesBanner = () => {
               <SpeakerphoneIcon className="h-6 w-6 text-white" aria-hidden="true" />
             </span>
             <p className="ml-3 font-medium text-white truncate">
-              <span className="md:hidden">Mudmap version 2 is here!! 🎉</span>
-              <span className="hidden md:inline">Mudmap <strong>version 2</strong> is here. Read up and get started today 🎉</span>
+              <span className="md:hidden"><span dangerouslySetInnerHTML={{__html: bannerText}}/></span>
+              {/*<span className="hidden md:inline">Mudmap <strong>version 2</strong> is here. Read up and get started today 🎉</span>*/}
+              <span className="hidden md:inline"><span dangerouslySetInnerHTML={{__html: mobileBannerText}}/></span>
             </p>
           </div>
-          <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
-            <a
-              href={blog}
-              className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
-            >
-              Learn more
-            </a>
-          </div>
+          {post ?
+            <div
+              className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+              <a
+                href={blog}
+                className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
+              >
+                Learn more
+              </a>
+            </div>
+            : <></>
+          }
           <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
             <button
               type="button"
